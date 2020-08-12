badf0609902f4b5991d0481a8537236a52b4702f
wee <416383444@qq.com>
2020-08-13 00:27:46 +0800

    字符类

---
+++ jdk/src/main/java/com/company/jdk/字符类/Demo.java
@@ -0,0 +1,21 @@
+package com.company.jdk.字符类;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        /*
+        * StringBuffer和StringBuilder相比String，都是长度可变的字符串对象；String指向常量池
+        * String和StringBuffer是线程安全的，String被final修饰，是不可变的，StringBuffer的大部分方法都有synchronized修饰；
+        * StringBuilder是线程不安全的
+        * */
+        String weezy = "weezy";
+        StringBuffer riri = new StringBuffer("riri");
+        StringBuilder fit = new StringBuilder("fit");
+        
+        //字符串追加
+        StringBuffer ririAppend = riri.append(weezy);
+        System.out.println(ririAppend.toString());
+        StringBuilder fitAppend = fit.append(weezy);
+        System.out.println(fitAppend.toString());
+    }
+}
