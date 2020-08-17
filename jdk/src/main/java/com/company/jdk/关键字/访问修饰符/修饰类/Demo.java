3537912c057350c1d7f988e70fb4a9116edab6c1
wee <416383444@qq.com>
2020-08-17 23:15:07 +0800

    关键字

---
+++ jdk/src/main/java/com/company/jdk/关键字/访问修饰符/修饰类/Demo.java
@@ -0,0 +1,14 @@
+package com.company.jdk.关键字.访问修饰符.修饰类;
+
+/*
+* 访问修饰符：public,protected,default,private控制类的访问权限
+*
+* 访问权限:指可以在哪里实例化一个类(即是否有权访问到该类)
+*   public:则任何地方都能访问
+*   protected:则只能在同一个包下或者继承外部类的情况下访问
+*   default:则只能在同一个包下访问
+*   private:则只能在外部类的内部访问
+*
+* */
+public class Demo {
+}
